e6563bb635d0c88c71b181f2274d660c804902dd
CSH2315 <105338921+CSH2315@users.noreply.github.com>
2023-03-08 23:36:53 +0900

    Add files via upload

---
+++ profile.js
@@ -0,0 +1,5 @@
+// 접기/펼치기 기능 구현
+function fold_toggle(obj) {
+  if (obj.style.display == "none") obj.style.display = "block";
+  else if (obj.style.display == "block") obj.style.display = "none";
+}
